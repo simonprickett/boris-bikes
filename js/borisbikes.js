69d6d78db417294700279c0ec6fde9ca449f854b
Simon Prickett <simon@crudworks.org>
2017-07-18 18:22:55 -0700

    Initial commit.

---
+++ js/borisbikes.js
@@ -0,0 +1,64 @@
+// borisbikes.js
+
+const app = {
+	init: () => {
+		// Do any initialization...
+	},
+
+	loadBikeData: () => {
+		$.getJSON(
+			'https://api.tfl.gov.uk/bikepoint',
+			(data) => {
+				app.processBikeData(data);
+			}
+		);
+	},
+
+	processBikeData: (bikeData) => {
+		for (let bikeStation of bikeData) {
+			const bikeStationInfo = {};
+			let attrsFound = 0;
+
+			bikeStationInfo.name = bikeStation.commonName;
+			bikeStationInfo.latitude = bikeStation.lat;
+			bikeStationInfo.longitude = bikeStation.lon;
+
+			for (let prop of bikeStation.additionalProperties) {
+				switch (prop.key) {
+					case 'NbBikes':
+						bikeStationInfo.numAvailableBikes = parseInt(prop.value);
+						attrsFound++;
+						break;
+
+					case 'NbEmptyDocks':
+						bikeStationInfo.numEmptyDocks = parseInt(prop.value);
+						attrsFound++;
+						break;
+
+					case 'NbDocks':
+						bikeStationInfo.numDocks = parseInt(prop.value);
+						attrsFound++;
+						break;
+				}
+
+				// If we have found everything we need, no need to keep 
+				// looking at other attrs...
+
+				if (attrsFound === 3) {
+					break;
+				}
+			} 
+
+			app.displayBikeStation(bikeStationInfo);
+		}
+	},
+
+	displayBikeStation: (bikeStationInfo) => {
+		$('#bike-station-list').append(`<li>${bikeStationInfo.name}, latitude: ${bikeStationInfo.latitude}, longitude: ${bikeStationInfo.longitude}, available bikes: ${bikeStationInfo.numAvailableBikes}, empty docks: ${bikeStationInfo.numEmptyDocks}, total docks: ${bikeStationInfo.numDocks}</li>`);
+	}
+}
+
+$(document).ready(() => {
+    app.init();
+    app.loadBikeData();
+});
